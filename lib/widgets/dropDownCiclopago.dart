126990e5a42dbddc024f29d910ddf6adfbea101f
Antonio Herazo <antonioherazo7@hotmail.com>
2021-04-12 14:10:05 -0500

    se reduce el nombre de las variables del Provider en el archivo dropDownCicloPago

--- lib/widgets/dropDownCiclopago.dart
+++ lib/widgets/dropDownCiclopago.dart
@@ -44,28 +44,29 @@ class _DroppDownCiclopagoState extends State<DroppDownCiclopago> {
 $newValue${'es'}''';
 
               setState(() {
+                ItemData providerData = Provider.of<ItemData>(context);
                 newValue == 'Semanal'
-                    ? Provider.of<ItemData>(context).cicloPago = 56
+                    ? providerData.cicloPago = 56
                     : newValue == 'Semanal'
-                        ? Provider.of<ItemData>(context).cicloPago = 112
-                        : Provider.of<ItemData>(context).cicloPago = 224;
+                        ? providerData.cicloPago = 112
+                        : providerData.cicloPago = 224;
                 //---------
                 valueChoosen = newValue;
-                if (Provider.of<ItemData>(context).ingresoFijoExist == false) {
+                if (providerData.ingresoFijoExist == false) {
                   final fixIncome = ItemModel(
                     name: 'Ingreso Fijo',
                     factor: 1,
                     factorPor: 1,
                     columnFactor: descripFactor,
                   );
-                  Provider.of<ItemData>(context).ingresoFijoExist = true;
-                  Provider.of<ItemData>(context).incomeList.add(fixIncome);
+                  providerData.ingresoFijoExist = true;
+                  providerData.incomeList.add(fixIncome);
                 }
                 // Incvocamos la funcion upDateItem de Provider para actualizar cambio de cicloPago
-                Provider.of<ItemData>(context).updateItem(
-                    Provider.of<ItemData>(context).incomeList[0],
-                    Provider.of<ItemData>(context).cicloPago.toString());
-                Provider.of<ItemData>(context).updateTotal();
+                providerData.updateItem(
+                    providerData.incomeList[0],
+                    providerData.cicloPago.toString());
+                providerData.updateTotal();
               });
             },
             //---------------------
