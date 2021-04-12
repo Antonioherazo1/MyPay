52923024fb8622a1193270fbeccc4898c1bcae83
Antonio Herazo <antonioherazo7@hotmail.com>
2021-04-12 12:30:43 -0500

    Se añade la palabra -es- a la terminacion de los tipos de ciclo de pagos

--- lib/widgets/dropDownCiclopago.dart
+++ lib/widgets/dropDownCiclopago.dart
@@ -40,8 +40,8 @@ class _DroppDownCiclopagoState extends State<DroppDownCiclopago> {
             }).toList(),
             //---------------------
             onChanged: (newValue) {
-              String descripFactor = '''Horas por
-$newValue''';
+              String descripFactor = '''Horas
+$newValue${'es'}''';
 
               setState(() {
                 newValue == 'Semanal'
