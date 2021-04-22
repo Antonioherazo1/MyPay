acdda856586eddd7cde8fda0353ab5da37d6db34
Antonio Herazo <antonioherazo7@hotmail.com>
2021-04-21 21:36:58 -0500

    Se añade boton de guardar Mes en pantalla Balance

--- lib/screens/balance.dart
+++ lib/screens/balance.dart
@@ -28,17 +28,19 @@ class _BalanceGeneralState extends State<BalanceGeneral> {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
+                  //--- Pago total --------
                   Totalizador(
                     total: Provider.of<ItemData>(context).totalizador,
                   ),
                   Padding(
                     padding: const EdgeInsets.symmetric(
                         vertical: 5.0, horizontal: 0.0),
+                    // --- Tìtulo Ingresos ----
                     child: Text('Ingresos'.toUpperCase(),
                         style: TextStyle(
                             fontSize: 20.0, fontWeight: FontWeight.bold)),
                   ),
-                  //------- ListBuilder ------
+                  //------- Lista Ingresos ------
                   ConstrainedBox(
                     constraints:
                         BoxConstraints(maxHeight: 200, minHeight: 56.0),
@@ -57,20 +59,22 @@ class _BalanceGeneralState extends State<BalanceGeneral> {
                               titleBold: false);
                         }),
                   ),
+                  //---Total Ingresos ------
                   BalanceITEM(
                       name: 'Total Ingresos: ',
                       value: Provider.of<ItemData>(context).sumIncome,
                       titleBold: true),
-                  // Container of the list of widgets with the information of Income and Egress
+                  // ---Linea Divisoria-----
                   Divider(height: 15.0, thickness: 5.0),
                   Padding(
                     padding: const EdgeInsets.symmetric(
                         vertical: 5.0, horizontal: 0.0),
+                    //--- Titulo Egresos -----
                     child: Text('Egresos'.toUpperCase(),
                         style: TextStyle(
                             fontSize: 20.0, fontWeight: FontWeight.bold)),
                   ),
-                  //------- ListBuilder ------
+                  //------- Lista De Ingresos ------
                   ConstrainedBox(
                     constraints:
                         BoxConstraints(maxHeight: 200, minHeight: 56.0),
@@ -90,6 +94,7 @@ class _BalanceGeneralState extends State<BalanceGeneral> {
                           );
                         }),
                   ),
+                  // ---- Total Egresos -----
                   BalanceITEM(
                     name: 'Total Egresos: ',
                     value: Provider.of<ItemData>(context).sumEgress,
@@ -98,67 +103,124 @@ class _BalanceGeneralState extends State<BalanceGeneral> {
                 ],
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.only(top: 20.0),
-              child: Container(
-                width: double.infinity,
-                height: 60.0,
-                child: RaisedButton(
-                    // padding: EdgeInsets.all(30.0),
-                    child: Text('Guardar valores del Ciclo',
-                        textAlign: TextAlign.center,
-                        style:
-                            TextStyle(color: Colors.white70, fontSize: 24.0)),
-                    color: Colors.blue,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10.0)),
-                    onPressed: () {
-                      showDialog(
-                          context: context,
-                          builder: (context) => AlertDialog(
-                                title: RichText(
-                                  text: TextSpan(
-                                    children: [
-                                      TextSpan(
-                                        text: ' Se va a Guardar el ',
-                                        style: TextStyle(
-                                            color: Colors.black54,
-                                            fontSize: 22.0),
-                                      ),
-                                      TextSpan(
-                                        text: ' Ciclo de Pago'.toUpperCase(),
-                                        style: TextStyle(
-                                            color: Colors.blue[300],
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 26.0),
-                                      ),
-                                    ],
-                                  ),
+            // --- Boton Guardar Datos de Ciclo ----
+            Container(
+              padding: EdgeInsets.only(top: 20.0),
+              width: double.infinity,
+              height: 60.0,
+              child: RaisedButton(
+                  child: Text('Guardar valores del Ciclo',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.white70, fontSize: 24.0)),
+                  color: Colors.blue,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0)),
+                  onPressed: () {
+                    //--- ventana Emergente GUARDAR CICLO PAGO----
+                    showDialog(
+                        context: context,
+                        builder: (context) => AlertDialog(
+                              title: RichText(
+                                text: TextSpan(
+                                  children: [
+                                    TextSpan(
+                                      text: ' Se va a Guardar el \n',
+                                      style: TextStyle(
+                                          color: Colors.black54,
+                                          fontSize: 22.0),
+                                    ),
+                                    TextSpan(
+                                      text: ' Ciclo de Pago'.toUpperCase(),
+                                      style: TextStyle(
+                                          color: Colors.blue[300],
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 26.0),
+                                    ),
+                                  ],
                                 ),
-                                content: Text(
-                                  'Confirma ?',
-                                  style: TextStyle(color: Colors.black54),
+                              ),
+                              content: Text(
+                                'Confirma ?',
+                                style: TextStyle(
+                                    color: Colors.black54, fontSize: 26.0),
+                              ),
+                              actions: [
+                                FlatButton(
+                                    onPressed: () {
+                                      Provider.of<ItemData>(context)
+                                          .saveCiclo();
+                                      Navigator.of(context).pop();
+                                    },
+                                    child: Text('Aceptar',
+                                        style: TextStyle(fontSize: 25.0))),
+                                FlatButton(
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                    child: Text('Cancelar',
+                                        style: TextStyle(fontSize: 25.0)))
+                              ],
+                            ));
+                  }),
+            ),
+            Container(
+              padding: EdgeInsets.only(top: 20.0),
+              width: double.infinity,
+              height: 60.0,
+              child: RaisedButton(                  
+                  child: Text('Guadar Datos Mes ',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.white70, fontSize: 24.0)),
+                  color: Colors.green[400],
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0)),
+                  onPressed: (){
+                     //--- ventana Emergente GUARDAR DATOS MES----
+                    showDialog(
+                        context: context,
+                        builder: (context) => AlertDialog(
+                              title: RichText(
+                                text: TextSpan(
+                                  children: [
+                                    TextSpan(
+                                      text: ' Se va a Guardar el \n',
+                                      style: TextStyle(
+                                          color: Colors.black54,
+                                          fontSize: 22.0),
+                                    ),
+                                    TextSpan(
+                                      text: 'mes'.toUpperCase(),
+                                      style: TextStyle(
+                                          color: Colors.blue[300],
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 26.0),
+                                    ),
+                                  ],
                                 ),
-                                actions: [
-                                  FlatButton(
-                                      onPressed: () {
-                                        Provider.of<ItemData>(context)
-                                            .saveCiclo();
-                                        Navigator.of(context).pop();
-                                      },
-                                      child: Text('Aceptar',
-                                          style: TextStyle(fontSize: 25.0))),
-                                  FlatButton(
-                                      onPressed: () {
-                                        Navigator.of(context).pop();
-                                      },
-                                      child: Text('Cancelar',
-                                          style: TextStyle(fontSize: 25.0)))
-                                ],
-                              ));
-                    }),
-              ),
-            )
+                              ),
+                              content: Text(
+                                'Confirma ?',
+                                style: TextStyle(
+                                    color: Colors.black54, fontSize: 26.0),
+                              ),
+                              actions: [
+                                FlatButton(
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                    child: Text('Aceptar',
+                                        style: TextStyle(fontSize: 25.0))),
+                                FlatButton(
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                    child: Text('Cancelar',
+                                        style: TextStyle(fontSize: 25.0)))
+                              ],
+                            ));
+                  }),
+            ),
+            
           ],
         ),
       ),
@@ -198,6 +260,16 @@ class BalanceITEM extends StatelessWidget {
           Text('$value', style: TextStyle(fontSize: 20.0))
         ],
       ),
+    );
+  }
+}
+
+class PopUpWindowConfirmation extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+      
     );
   }
 }
--- lib/screens/estadistica.dart
+++ lib/screens/estadistica.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:mi_pago/models/itemData.dart';
+import 'package:mi_pago/models/cicloDataModel.dart';
 
 class Estadistica extends StatefulWidget {
   @override
@@ -6,13 +9,51 @@ class Estadistica extends StatefulWidget {
 }
 
 class _EstadisticaState extends State<Estadistica> {
+  List<String> lista1 = ['item1', 'item2', 'item3', 'item4', 'item5'];
+  List<String> lista2 = ['item10', 'item20', 'item30', 'item40', 'item50'];
+  String valChoosen1;
+  String valChoosen2;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Estadistica'),
       ),
-      
+      body: Container(
+        // child: ListView.builder(
+        //   itemCount: Provider.of<ItemData>(context).ciclosDataList.length,
+        //   itemBuilder: (context, index){
+        //     ListView.builder(
+        //       itemCount: Provider.of<ItemData>(context).ciclosDataList.
+        //       itemBuilder: null)
+        //   }
+        //   ),
+
+        child: DropdownButton(
+          hint: Text('Elige el ciclo'),
+          value: valChoosen1,
+          // items: Provider.of<ItemData>(context).ciclosDataList.map((ciclo) {
+          items: lista1.map((e){
+            return DropdownMenuItem(
+              // value: ciclo.date,
+              value: e,
+              child: Expanded(
+                // width: 150.0,
+              // child: Text('${ciclo.dateString} - \$ ${ciclo.total}'),
+              child: Text('$e'),
+              ),
+            );
+          }).toList(),
+          //---------------------
+          onChanged: (newValue) {
+            setState(() {
+              valChoosen1 = newValue;
+            });
+          },
+          //---------------------
+        ),
+      ),
     );
   }
 }
